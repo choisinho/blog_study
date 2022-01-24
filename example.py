d2636504e8a5c69d51d621dc5239f1c1f758c51d
Sinho <choi_sinho@naver.com>
2022-01-24 16:18:49 +0900

    Update example.py

--- example.py
+++ example.py
@@ -1,10 +1,2 @@
-vips = ["홍길동", "청길동", "녹길동", "황길동"]
-who = input("찾을 멤버는?")
-
-for vip in vips: #무한루프
-    if vip == who:
-        print("찾았다... {}!!".format(vip))
-        break
-    else:
-        continue
-    print("{}를 비교하였음.".format(vip))
+l = [1, 2, 3, 4, 5]
+print("reversed list : ", reversed(l)) 
